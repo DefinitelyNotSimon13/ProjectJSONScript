421d41f0417d9165035500337411f16653b08246
Simon <simon21.blum@gmail.com>
2024-03-14 14:22:47 +0100

    Add script for retrieving projects

--- getProjects.sh
+++ getProjects.sh
@@ -1,10 +1,11 @@
-JSONS=$(curl -X GET http://127.0.0.1:8000/project/all)
-echo "Received:"
-echo $JSONS | jq > jsons.json
-LENGTH=$(jq length jsons.json)
-echo Length: $LENGTH
-mkdir projects
+JSONS=$(curl -sX GET http://127.0.0.1:8000/project/all)
+echo $JSONS | jq > .jsons.json
+LENGTH=$(jq length .jsons.json)
+mkdir projects -p
 for i in $(seq 0 $(($LENGTH-1))); do
-    name=$(echo $JSONS | jq .[$i] | jq '.name' | tr -d '"' | tr -d ' ' | tr -d '\n' | tr -d ' ')
+    name=$(echo $JSONS | jq .[$i] | jq '.name' | tr -d '"' | tr -d ' ' | tr -d '\n')
     echo $JSONS | jq .[$i] 0> projects/$name.json
 done
+echo "Retrieved $LENGTH projects."
+echo "Saved to projects/ directory."
+rm .jsons.json
